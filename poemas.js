e2d8f531e5add8b7870fd45ee629a32b3a97c33e
Luis Alberto Izquierdo Rojas <66661619+albertsadday@users.noreply.github.com>
2025-04-19 01:55:58 -0600

    Create poemas.js

---
+++ poemas.js
@@ -0,0 +1,11 @@
+const poemsData = [
+    {
+        titulo: "Imprecación",
+        fragmento: "¡Vida, estúpida Vida, te arrojamos al rostro...",
+        texto: `¡Vida, estúpida Vida, te arrojamos al rostro
+como un escupitajo, nuestro insulto!...
+¡Eres perversa y cruel!... Y a pesar de todos tus afeites
+¡Trascendes a letrina y a sudor de loba siempre en branza!...`
+    },
+    // Más poemas
+];
